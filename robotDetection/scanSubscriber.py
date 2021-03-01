97ec52d3846b258b6f0093f083a06c5baa6a37cb
andrewsimonds14 <andrew.simonds14@hotmail.com>
2021-02-28 19:39:41 -0500

    Changed up scanSubscriber to be able to maintain a certain velocity (essentially cruise control following a car)

--- robotDetection/scanSubscriber.py
+++ robotDetection/scanSubscriber.py
@@ -5,6 +5,13 @@ from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
+from simple_pid import PID
+
+# Setting global PID parameters
+pid = PID(0.5, 0.01, 0.005, setpoint = 0)
+
+# Define the follow distance for a robot to keep
+followDistance = 3.0
 
 class scanNode(Node):
     def __init__(self):
@@ -12,9 +19,15 @@ class scanNode(Node):
         self.scan_subscriber = self.create_subscription(LaserScan, '/scan', self.callback, qos_profile_sensor_data)
         self.velocity_publisher = self.create_publisher(Twist, "cmd_vel", 10)
 
-    def publish_speed(self,speed):
+    def publish_PID_speed(self,distance):
         velocity = Twist()
-        velocity.linear.x = speed
+        velocity.linear.x = pid(followDistance - distance) #The three is an arbitrary number indicating the distance to keep from the object in front
+        velocity.angular.z = 0.0
+        self.velocity_publisher.publish(velocity)
+
+    def publish_default_speed(self):
+        velocity = Twist()
+        velocity.linear.x = 0.4
         velocity.angular.z = 0.0
         self.velocity_publisher.publish(velocity)
 
@@ -28,12 +41,11 @@ class scanNode(Node):
         dist_head = format(msg.ranges[0], '.2f')
         print('Distance Front: ', dist_head)
 
-        # Stop if something in front
-        # Change to use PID when trying to cruise behind another vehicle instead of stop
-        if float(dist_head) < 3.0:
-            self.publish_speed(0.0)
+        # Check for follow distance stuff if something is approaching
+        if float(dist_head) < followDistance + 2.0:
+            self.publish_PID_speed(float(dist_head))
         else:
-            self.publish_speed(0.4)
+            self.publish_default_speed()
 
  
 def main(args = None):
